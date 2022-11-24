c3a8392a90426e6c1d6c61b22628c1b26914c45c
Solomon Mithra <solomonmithra99@gmail.com>
2022-11-24 10:24:37 +0530

    prime number

--- 8.2/index.js
+++ 8.2/index.js
@@ -1,9 +1,57 @@
-// program to check if a number is prime or not
+function isPrimeNumber(number)
+{   
+    let isPrime = true;
+    if (number === 1) {
+    console.log("1 is neither prime nor composite number.");
+    }
+
+    else if (number > 1) {
+        for (let i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    else {
+        return true;
+    }
+}
+
+function nextPrime(N)
+{
+    if (N <= 0){
+        console.log(`1 is the next prime number`);
+        return 1;    
+    }
+
+    if (N <= 1){
+        console.log(`2 is the next prime number`);
+        return 2;    
+    }
+    let prime = N;
+    let found = false;
+    while (!found) {
+        prime++;
+ 
+        if (isPrimeNumber(prime))
+            found = true;
+    }
+    console.log(`${prime} is the next prime number`);
+    return prime;
+}
+
+
 const prompt=require("prompt-sync")({sigint:true}); 
 
 // take input from the user
 const number = (prompt("Enter a positive number: "));
-let isPrime = true;
 
 // check if number is equal to 1
 if (number === 1) {
@@ -13,15 +61,7 @@ if (number === 1) {
 // check if number is greater than 1
 else if (number > 1) {
 
-    // looping through 2 to number-1
-    for (let i = 2; i < number; i++) {
-        if (number % i == 0) {
-            isPrime = false;
-            break;
-        }
-    }
-
-    if (isPrime) {
+    if (isPrimeNumber(number)) {
         console.log(`${number} is a prime number`);
     } else {
         console.log(`${number} is a not prime number`);
@@ -32,3 +72,12 @@ else if (number > 1) {
 else {
     console.log("The number is not a prime number.");
 }
+
+// the next prime number
+var nxtNumber = nextPrime(number);
+
+// Difference between number and next prime number
+var diff = nxtNumber - parseInt(number); 
+
+console.log("The difference between number and next prime number")
+console.log(`${nxtNumber} - ${number} = ${diff}`)
